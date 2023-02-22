54d48db763fbf00f0364fce0f2ffd820e9a29c9a
nathgrs6 <nathan.grosjean@etu.univ-smb.fr>
2023-02-22 08:27:51 +0100

    Ajoutez des fichiers projet.

---
+++ TP3console/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TP3console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
